135c75d5fa6d1f343cef60f05568c81b195e2a73
Jad Saad <jadsaad06@gmail.com>
2025-01-28 14:22:00 -0800

    Project foundation set up, backend now connects to mongoDB through an express server and mongoose. Frontend pulls up a blank page, but connects with the database and server successfully

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const uri = "mongodb+srv://jadsaad:jadsaad@user-data.txvyv.mongodb.net/?retryWrites=true&w=majority&appName=User-Data"
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from MERN stack!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
+
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
