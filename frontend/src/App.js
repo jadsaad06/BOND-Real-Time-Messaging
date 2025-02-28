d63b189e802950175a07cba1d291968048646e5b
Jad Saad <jadsaad06@gmail.com>
2025-02-27 18:14:34 -0800

    Feat: Added Header
    Header includes profile picture, username, title, and settings button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,22 @@
-import React, { useState } from 'react';
 import './App.css';
+import { FaCog } from 'react-icons/fa';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  const handleSendMessage = () => {
-    if (message.trim()) {
-      setMessages(prevMessages => [
-        ...prevMessages,
-        { id: Math.random().toString(), text: message }
-      ]);
-      setMessage(''); // Clear the input field after sending
-    }
-  };
-
   return (
-        <div className='container'>
-          <div className="app-container">
-            <div className="header">
-              <h1>Instant Messaging</h1>
-            </div>
-      
-            <div className="messages-container">
-              {messages.map(msg => (
-                <div key={msg.id} className="message">
-                  {msg.text}
-                </div>
-              ))}
-            </div>
-      
-            <div className="input-container">
-              <input
-                type="text"
-                className="message-input"
-                placeholder="Type a message..."
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <button onClick={handleSendMessage} className="send-button">
-                Send
-              </button>
-            </div>
-          </div>
+    <div className="App">
+      <header className="main-container">
+        <profile className="profile-container">
+          <button className="profile-button">
+            <img className="pfp" src="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=" alt="Profile Picture" />
+            <h2>James Bond</h2>
+          </button>
+        </profile>
+        <div className="title-container">
+          <h1 className='title'>Bond</h1>
         </div>
+        <FaCog className="settings-icon" />
+      </header>
+    </div>
   );
 }
 
